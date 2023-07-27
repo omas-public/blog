80654c53ac9bf464d3a5b2e9c54e8b473e073983
Omas Naohiko <omas@std.it-college.ac.jp>
2023-07-27 21:47:37 +0900

    2.9 image on off

--- components/hero.js
+++ components/hero.js
@@ -1,7 +1,8 @@
-const Hero = ({ title, subtitle }) => (
+const Hero = ({ title, subtitle, imageOn = false }) => (
   <div>
     <h1>{title}</h1>
     <p>{subtitle}</p>
+    {imageOn && <figure>[image]</figure>}
   </div>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Hero from "components/hero";
 const params = {
   title: "CUBE",
   subtitle: "アウトプットしていくサイト",
+  imageOn: true,
 };
 const Home = () => {
   return (
