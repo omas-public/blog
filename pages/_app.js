fe09ce20b22eb82b86022771e8b955afd0392a98
Omas Naohiko <omas@std.it-college.ac.jp>
2023-08-10 20:31:02 +0900

    fix global style

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import Layout from 'components/layout'
-// import '@/styles/globals.css'
+import '@/styles/globals.css'
 
-const App = ({ Component, pageProps }) => {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
-}
+const App = ({ Component, pageProps }) => (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+)
 
 export default App
