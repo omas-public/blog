e25d9495102aac6d6279d336ae89d9d47de5f4bb
Omas Naohiko <omas@std.it-college.ac.jp>
2023-08-14 14:36:55 +0900

    3.6 logo style

--- components/logo.js
+++ components/logo.js
@@ -1,5 +1,10 @@
 import Link from 'next/link'
+import styles from 'styles/logo.module.css'
 
-const Logo = () => <Link href='/'>CUBE</Link>
+const Logo = ({ boxOn = false }) => (
+  <Link href='/' className={boxOn ? styles.box : styles.basic}>
+    CUBE
+  </Link>
+)
 
 export default Logo
